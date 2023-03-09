f15b4ad415571004d3e94f94120bc27b257954e3
Thibault <76521322+thibault0712@users.noreply.github.com>
2023-03-09 20:37:32 +0100

    Update and rename FinalProject.ino to Drive-motor.ino

--- Drive-motor.ino
+++ Drive-motor.ino
@@ -2,12 +2,14 @@ int relay = 3;
 int relay2 = 4;
 int timeMaxMotor = 4000;
 int sizeMaxMotor = 40;
+int timeMotorPush = 0;
 
 void setup()
 {
   pinMode(relay, OUTPUT);
   pinMode(relay2, OUTPUT);
-  actionMotor(10);
+  //actionMotor(10); -> ajuster la hauteur de l'antivol pour un vélo ayant un rayon de 10cm
+  //resetMotor(); -> Remettre le verrain à 0
 }
 
 
@@ -19,9 +21,17 @@ void loop()
 
 void actionMotor(int bicicleSize){
   int sizeMotorPush = sizeMaxMotor - bicicleSize;
-  int timeMotorPush = timeMaxMotor/sizeMaxMotor*sizeMotorPush;
+  timeMotorPush = timeMaxMotor/sizeMaxMotor*sizeMotorPush;
   digitalWrite(relay2, HIGH);
   digitalWrite(relay, LOW);
+  delay(timeMotorPush);
+  digitalWrite(relay2, LOW);
+  digitalWrite(relay, LOW);
+}
+
+void resetMotor(){
+  digitalWrite(relay2, LOW);
+  digitalWrite(relay, HIGH);
   delay(timeMotorPush);
   digitalWrite(relay2, LOW);
   digitalWrite(relay, LOW);
